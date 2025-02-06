2f798fdbf066aae68809afc05e9bf59f5825904a
Rooney kalumba <22ki129@esisalama.org>
2025-02-06 09:50:05 +0200

    Fix the encode and decode function

--- app/src/main/java/com/example/encodeurdecodeur/MainActivity.kt
+++ app/src/main/java/com/example/encodeurdecodeur/MainActivity.kt
@@ -1,24 +1,39 @@
 package com.example.encodeurdecodeur
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.BasicTextField
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+    @OptIn(ExperimentalMaterial3Api::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CesarCipherApp()
+                Scaffold(
+                    topBar = {
+                        TopAppBar(
+                            title = { Text("Encodeur De Cesar") },
+                        )
+                    }
+                ) {
+                    CesarCipherApp()
+
+                    }
+                }
+            }
+
         }
-    }
-}
+
+
 
 @Composable
 fun CesarCipherApp() {
@@ -26,7 +41,9 @@ fun CesarCipherApp() {
     var decalage by remember { mutableStateOf("3") }
     var resultat by remember { mutableStateOf("") }
 
-    Column(modifier = Modifier.padding(16.dp)) {
+    Column(
+
+        modifier = Modifier.padding(16.dp)) {
         Text("Encodeur et Décodeur de César", style = MaterialTheme.typography.headlineSmall)
 
         Spacer(modifier = Modifier.height(16.dp))
@@ -34,8 +51,8 @@ fun CesarCipherApp() {
         TextField(
             value = texte,
             onValueChange = { texte = it },
-            modifier = Modifier.fillMaxWidth(),
-
+            label = {Text("Entrez le text a coder")},
+            modifier  = Modifier.width(200.dp).align(Alignment.CenterHorizontally)
 
         )
 
@@ -44,30 +61,38 @@ fun CesarCipherApp() {
         TextField(
             value = decalage,
             onValueChange = { decalage = it },
-            modifier = Modifier.fillMaxWidth()
+            label = {Text("Decalage")},
+            modifier  = Modifier.width(200.dp).align(Alignment.CenterHorizontally)
+            ,
+
 
         )
         Spacer(modifier = Modifier.height(16.dp))
 
+        Row(modifier = Modifier.align(Alignment.CenterHorizontally)){
         Button(onClick = {
             val shift = decalage.toIntOrNull() ?: 3
             resultat = CesarCipher.encoder(texte, shift)
         },
-            modifier = Modifier.fillMaxWidth()) {
+            modifier = Modifier.width(100.dp)) {
             Text("Encoder")
         }
+        Spacer(modifier = Modifier.width(16.dp))
 
         Button(onClick = {
             val shift = decalage.toIntOrNull() ?: 3
             resultat = CesarCipher.decoder(texte, shift)
         },
-            modifier = Modifier.fillMaxWidth()) {
+            modifier = Modifier.width(100.dp)
+            ) {
             Text("Décoder")
+        }
         }
 
         Spacer(modifier = Modifier.height(16.dp))
 
-        Text("Résultat : $resultat", style = MaterialTheme.typography.bodyLarge)
+        Text("Résultat : $resultat", style = MaterialTheme.typography.bodyLarge, modifier = Modifier.fillMaxWidth().align(Alignment.CenterHorizontally))
+
     }
 }
 
